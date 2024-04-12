6acb4c83ec25a225983410fab8f713ad785b128b
yijian166 <iwcc@outlook.com>
2024-04-12 08:43:22 +0800

    feat: scrape url

---
+++ scrape_url/src/main.rs
@@ -0,0 +1,65 @@
+use std::fs;
+
+fn main1() {
+    let url = "https://www.rust-lang.org/";
+    let output = "rust.md";
+
+    println!("Fetch url:{}", url);
+    let body = reqwest::blocking::get(url).unwrap().text().unwrap();
+
+    println!("Converting html to markdown...");
+    let md = html2md::parse_html(&body);
+
+    fs::write(output, md.as_bytes()).unwrap();
+    println!("Converted markdown has been saved in {}.", output);
+}
+
+fn fib_loop(n: u8) {
+    let mut a = 1;
+    let mut b = 1;
+    let mut i = 2u8;
+
+    loop {
+        let c = a + b;
+        a = b;
+        b = c;
+        i += 1;
+
+        println!("next val is {}", b);
+
+        if i >= n {
+            break;
+        }
+    }
+}
+
+fn fib_while(n: u8) {
+    let (mut a, mut b, mut i) = (1, 1, 2);
+
+    while i < n {
+        let c = a + b;
+        a = b;
+        b = c;
+        i += 1;
+
+        println!("next val is {}", b);
+    }
+}
+
+fn fib_for(n: u8) {
+    let (mut a, mut b) = (1, 1);
+
+    for _ in 2..n {
+        let c = a + b;
+        a = b;
+        b = c;
+        println!("next val is {}", b);
+    }
+}
+
+fn main() {
+    let n = 10;
+    fib_loop(n);
+    fib_while(n);
+    fib_for(n);
+}
