5a5adc2d867ae3598c3ddb8d5a34f18834e722eb
yijian166 <iwcc@outlook.com>
2024-04-18 08:20:36 +0800

    fix: fix type error cause wrong anyhow::ok import

--- thumbor/src/main.rs
+++ thumbor/src/main.rs
@@ -1,8 +1,6 @@
-use anyhow::Ok;
-use axum::{extract::Path, routing::get, http::StatusCode, Router};
+use axum::{extract::Path, http::StatusCode, routing::get, Router};
 use percent_encoding::percent_decode_str;
 use serde::Deserialize;
-use std::convert::TryInto;
 
 mod pb;
 
@@ -19,25 +17,21 @@ struct Params {
 async fn main() {
     tracing_subscriber::fmt::init();
 
-    let app = Router::new()
-        .route("/image/:spec/:url", get(generate));
+    let app = Router::new().route("/image/:spec/:url", get(generate));
 
-    let addr:String = "127.0.0.1:3000".parse().unwrap();
-    tracing::debug!("listening on {}",addr);
+    let addr: String = "127.0.0.1:3000".parse().unwrap();
+    tracing::debug!("listening on {}", addr);
     let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
 
-
-async fn generate(Path(Params {spec, url}): Path<Params>) -> Result<String,StatusCode> {
+async fn generate(Path(Params { spec, url }): Path<Params>) -> Result<String, StatusCode> {
     let url = percent_decode_str(&url).decode_utf8_lossy();
-    //.try_into().map_err(|_| StatusCode::BAD_REQUEST)?;
-    //.try_into().map_err(|_| StatusCode::BAD_REQUEST)?;
-    let spec:ImageSpec = spec
+    // let url = "xx".to_string();
+    let spec: ImageSpec = spec
         .as_str()
         .try_into()
         .map_err(|_| StatusCode::BAD_REQUEST)?;
-    let res = format!("url: {}\n spec: {:#?}",url, spec);
-    //.try_into().map_err(|_| StatusCode::BAD_REQUEST)?;
+    let res = format!("url: {}\n spec: {:#?}", url, spec);
     Ok(res)
 }
